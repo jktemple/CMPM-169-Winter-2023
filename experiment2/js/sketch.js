3d66099f02eb781d5344269be72bdb4976aee4dd
jktemple <jktemple@ucsc.edu>
2023-01-23 21:45:33 -0800

    Experiment 2

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -25,43 +25,92 @@ class MyClass {
     }
 }
 
-// setup() function is called once when the program starts
+// P_2_0_03
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * drawing with a changing shape by draging the mouse.
+ *
+ * MOUSE
+ * position x          : length
+ * position y          : thickness and number of lines
+ * drag                : draw
+ *
+ * KEYS
+ * 1-3                 : stroke color
+ * del, backspace      : erase
+ * s                   : save png
+ */
+'use strict';
+
+var strokeColor;
+var numberOfSides;
+
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(720, 720);
+  colorMode(HSB, 360, 100, 100, 100);
+  noFill();
+  strokeWeight(2);
+  strokeColor = color(0, 10);
+  numberOfSides = 3;
+  frameRate(60);
+  //angleMode(DEGREES);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  //strokeColor = color(random(0,255), random(0,255), random(0,255), random(0,255));
+  if (mouseIsPressed && mouseButton == LEFT) {
+    push();
+    translate(mouseX + cos(frameCount)*100, mouseY + sin(frameCount)*100);
+    rotate(frameCount/10000);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    //circleResolution = int(map(mouseY + 100, 0, height, 2, 10));
+    var radius = mouseX - width / 2;
+    var angle = TAU / numberOfSides;
+
+    stroke(strokeColor);
+    
+    beginShape();
+    for (var i = 0; i <= numberOfSides; i++) {
+      var x = cos(angle * i) * radius;
+      var y = sin(angle * i) * radius;
+      vertex(x, y);
+    }
+    endShape();    
+    pop();
+  }
+  //angleMode(DEGREES);
+  //rotate(2);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function keyReleased() {
+  if (keyCode == DELETE || keyCode == BACKSPACE) background(0, 0, 100);
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+
+  //if (key == '1') strokeColor = color(0, 10);
+  if (key == '2') numberOfSides = 2;
+  if (key == '3') numberOfSides = 3;
+  if (key == '4') numberOfSides = 4;
+  if (key == '5') numberOfSides = 5;
+  if (key == '6') numberOfSides = 6;
+  if (key == '7') numberOfSides = 7;
+  if (key == '8') numberOfSides = 8;
+  if (key == '9') numberOfSides = 9;
+  
 }
